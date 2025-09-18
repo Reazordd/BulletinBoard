7f4b9003465f500aa974b8d3af35e937e6d9b10a
Reazordd <126767396+Reazordd@users.noreply.github.com>
2025-09-18 23:33:47 +0300

    деплой начало

--- BulletinBoard/settings.py
+++ BulletinBoard/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'DamnedS.pythonanywhere.com',  # Замените yourusername на ваше имя пользователя
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 INSTALLED_APPS = [
